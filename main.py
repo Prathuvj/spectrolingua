4dd1031ddab483d60a407c6d3016ed59e5c3e4d6
Prathuvj <vijayvargiyaprathu@gmail.com>
2025-08-07 18:28:28 +0530

    doc endpoint

--- main.py
+++ main.py
@@ -380,7 +380,7 @@ urlpatterns = [
     path('health/', health_check, name='health_check'),
     path('waveform/', generate_waveform, name='generate_waveform'),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
