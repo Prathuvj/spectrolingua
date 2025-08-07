360f63e59ea06ecd3a0dc75e7bc43f141ad95996
Prathuvj <vijayvargiyaprathu@gmail.com>
2025-08-07 17:35:31 +0530

    debugging-1

--- main.py
+++ main.py
@@ -9,11 +9,6 @@ from django.urls import path, re_path
 from django.core.files.uploadedfile import InMemoryUploadedFile
 import json
 from format_conversion import AudioConverter
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from drf_yasg.utils import swagger_auto_schema
-from drf_yasg import openapi
 
 
 if not settings.configured:
@@ -39,6 +34,12 @@ if not settings.configured:
         },
     )
     django.setup()
+
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 
 
 schema_view = get_schema_view(
